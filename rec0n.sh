dd590940380543a2be3c40468d03f3ca1e48e2d6
Rodrigo Padilha <151454973+pad1ryoshi@users.noreply.github.com>
2024-07-06 16:35:03 -0300

    Update rec0n.sh

--- rec0n.sh
+++ rec0n.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Definição de cores
+# colors
 RED='\e[31m'
 GREEN='\e[32m'
 YELLOW='\e[33m'
-NC='\e[0m' # Sem cor
+NC='\e[0m' # no color
 
 # Banner
 echo -e "${YELLOW}  ____ ____ ____ ____ ____ ____ ${NC}"
@@ -20,48 +20,48 @@ echo -e "${GREEN}!${NC}"
 echo
 echo
 
-# Verificar se os comandos necessários estão instalados
+# Verify that the necessary commands are installed
 if ! command -v subfinder &> /dev/null || ! command -v anew &> /dev/null || ! command -v assetfinder &> /dev/null || ! command -v httpx &> /dev/null; then
-    echo -e "${RED}Erro: subfinder, anew, assetfinder e/ou httpx não estão instalados. Por favor, instale-os e tente novamente.${NC}"
+    echo -e "${RED}Error: subfinder, anew, assetfinder and/or httpx are not installed. Please install them and try again.${NC}"
     exit 1
 fi
 
-read -p "Iniciar o processo de recon? [S/N]: " sn
+read -p "Start the recon process? [Y/N]: " yn
 echo
 echo
 
-if [[ ${sn^^} == "S" ]]; then
-    read -p "Digite o nome do domínio: " domain
+if [[ ${sn^^} == "Y" ]]; then
+    read -p "Enter the domain name: " domain
 
     if [ -z "$domain" ]; then
-        echo -e "${RED}Erro: Nenhum domínio fornecido.${NC}"
+        echo -e "${RED}Error: No domain provided.${NC}"
         exit 1
     fi
 
-    echo -e "${GREEN}INICIALIZANDO SUBFINDER EM: ${domain}${NC}"
+    echo -e "${GREEN}INITIALIZING SUBFINDER IN: ${domain}${NC}"
     subfinder -d ${domain} -all -silent | anew subf.txt
     echo
-    echo -e "${GREEN}subfinder finalizado [X]${NC}"
+    echo -e "${GREEN}finished subfinder [X]${NC}"
     echo
     echo
 
-    echo -e "${GREEN}INICIALIZANDO ASSETFINDER EM: ${domain}${NC}"
+    echo -e "${GREEN}INITIALIZING ASSETFINDER IN: ${domain}${NC}"
     assetfinder ${domain} -silent | anew asset.txt
     echo
-    echo -e "${GREEN}assetfinder finalizado [X]${NC}"
+    echo -e "${GREEN}finished assetfinder [X]${NC}"
     echo
     echo
 
-    echo -e "${GREEN}Criando arquivo subs.txt${NC}"
+    echo -e "${GREEN}Creating subs.txt file${NC}"
     cat subf.txt asset.txt | anew subs.txt
     rm subf.txt asset.txt
     echo
     echo
 
-    echo -e "${GREEN}Obtendo apenas status 200${NC}"
+    echo -e "${GREEN}Getting only status 200 and more juicy info${NC}"
     cat subs.txt | httpx -mc 200 -follow-host-redirects -random-agent -status-code -silent -retries 2 -title -web-server -tech-detect -location -o 200_info.txt
-    echo -e "${GREEN}Processo de recon finalizado.${NC}"
+    echo -e "${GREEN}Recon process completed.${NC}"
 else
-    echo -e "${YELLOW}Processo de recon cancelado pelo usuário.${NC}"
+    echo -e "${YELLOW}Recon process canceled by user.${NC}"
 fi
 
