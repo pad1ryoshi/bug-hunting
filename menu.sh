ddd684a92f627e5fb066ffed97c4d1670251b4c6
Rodrigo Padilha <151454973+pad1ryoshi@users.noreply.github.com>
2024-07-06 16:37:13 -0300

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-# Definição de cores
+# colors
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
-NC='\033[0m' # Sem cor
+NC='\033[0m' # no color
 
-# Função para o script de recon
+# Function for the recon script
 run_rec0n() {
-    echo -e "${GREEN}Iniciando script de recon...${NC}"
+    echo -e "${GREEN}Starting recon script...${NC}"
     ./rec0n.sh
 }
 
-# Função para o script de crawling
+# Function for the crawling script
 run_cr4wl() {
-    echo -e "${GREEN}Iniciando script de crawling...${NC}"
+    echo -e "${GREEN}Starting crawling script...${NC}"
     ./cr4wl.sh
 }
 
@@ -22,16 +22,16 @@ run_cr4wl() {
 # Menu interativo
 while true; do
     clear
-    echo -e "${YELLOW}Selecione uma opção:${NC}"
-    echo "1) Executar script de recon"
-    echo "2) Executar script de crawling"
-    echo "3) Sair"
-    read -p "Opção: " option
+    echo -e "${YELLOW}Select an option:${NC}"
+    echo "1) Run recon script"
+    echo "2) Run crawling script"
+    echo "3) Leave"
+    read -p "Option: " option
     case $option in
         1) run_rec0n ;;
         2) run_cr4wl ;;
-        3) echo -e "${GREEN}Saindo...${NC}" ; exit 0 ;;
-        *) echo -e "${RED}Opção inválida!${NC}" ; sleep 1 ;;
+        3) echo -e "${GREEN}Leaving...${NC}" ; exit 0 ;;
+        *) echo -e "${RED}Invalid option!${NC}" ; sleep 1 ;;
     esac
 done
 
